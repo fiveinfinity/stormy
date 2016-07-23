516c994fdd9bc272e52e31cba9eb41af8b0ad14f
fiveinfinity <jlachance1@gmail.com>
2016-07-23 10:19:01 -0500

    added logo, refactored to service

--- app/assets/javascripts/app/controllers/HomeController.js
+++ app/assets/javascripts/app/controllers/HomeController.js
@@ -1,5 +1,13 @@
-function HomeController($scope, uiGmapGoogleMapApi, uiGmapIsReady) {
+/* maps vs map vs $scope.map
+*  maps is a generic Google Maps object that you can create Directions and Displays on (via uiGmapGoogleMapApi).
+*  map is a returned promise of an actual map (via uiGmapIsReady).
+*  $scope.map holds parameters regarding how you want your map to display on your page.
+*/
+
+
+function HomeController($scope, uiGmapGoogleMapApi, uiGmapIsReady, MapsService) {
   var ctrl = this;
+  //sets $scope map parameters and creates an instance of Google Maps (you use this to instantiate services).
   $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8, control: {} };
   uiGmapGoogleMapApi.then(function(maps) {
     ctrl.maps = maps;
@@ -11,47 +19,17 @@ function HomeController($scope, uiGmapGoogleMapApi, uiGmapIsReady) {
       ctrl.directionsService = new ctrl.maps.DirectionsService();
       ctrl.map = instances[0].map;
       ctrl.directionsDisplay.setMap(ctrl.map);
-      ctrl.directionsAPI(ctrl.directionsService, ctrl.maps);
+      ctrl.createDirections(ctrl.directionsService, ctrl.maps);
     });
   }
 
-  ctrl.directionsAPI = function(directionsService, maps) {
-    directionsService.route({
-      origin: ctrl.origin,
-      destination: ctrl.destination,
-      travelMode: maps.TravelMode.DRIVING,
-      optimizeWaypoints: true,
-      drivingOptions: {
-        departureTime: new Date('2016-07-29'),
-        trafficModel: google.maps.TrafficModel.PESSIMISTIC
-      }
-    }, function(response, status) {
+  ctrl.createDirections = function(directionsService, maps) {
+    directionsService.route(MapsService.directionParams(ctrl.origin, ctrl.destination, maps),
+    function(response, status) {
       if (status == maps.DirectionsStatus.OK) {
+        //sets directions, creates markers on the maps object via MapsService
         ctrl.directionsDisplay.setDirections(response);
-//      IN THE RESPONSE (THIS IS THE POLYLINE AND DATA THAT CREATE THE ROUTE), A 'ROUTES' PARAMETER IS STORED.
-//      INSIDE OF THIS, IS THE OVERVIEW_PATH. THE LAT/LNG FROM EACH IS STORED, AND WE CAN PULL WEATHER EVERY 30 POINTS (OR HOWEVER MANY)
-//      THE FOLLOWING CODE RETRIEVES THE LAT & LNG DEPENDING ON THE INDEX OF THE OVERVIEW PATH.
-//         response['routes'][0]['overview_path']['INSERT INDEX HERE'].lat()
-//         response['routes'][0]['overview_path']['INSERT INDEX HERE'].lng()
-
-        // THE FOLLOWING CODE TAKES THE OVERVIEW PATH AND STORES LAT & LNG POINTS EVERY 30 KEYS IN THE OVERVIEW PATH
-        // STORED IN THE weatherPoint OBJECT. CREATES NEW MARKERS ALONG ROUTE AND RENDERS MARKERS.
-        // REFACTOR INTO A SERVICE
-        ctrl.weatherPoint = {};
-        routePoints = response['routes'][0]['overview_path'];
-        i = 0;
-
-        for(j = 30; j < routePoints.length; j+=30) {
-          lat = routePoints[j].lat();
-          lng = routePoints[j].lng();
-          ctrl.weatherPoint[i] = [routePoints[j].lat(), routePoints[j].lng()];
-          i++;
-
-          new maps.Marker({
-            position: {lat, lng},
-            map: ctrl.map
-          });
-        }
+        ctrl.weatherPoint = MapsService.createMarkers(response, maps, ctrl.map)
       }
     });
   }
--- app/assets/javascripts/app/services/MapsService.js
+++ app/assets/javascripts/app/services/MapsService.js
@@ -1,5 +1,61 @@
-function MapsService() {
+/* createDirections function
+*  this is returning the required object to create a directions route.
+*  Trying to keep our controllers thin, better for testing.
+*/
 
+
+/*  createMarkers function
+*   IN THE RESPONSE (THIS IS THE POLYLINE AND DATA THAT CREATE THE ROUTE), A 'ROUTES' PARAMETER IS STORED.
+*   INSIDE OF THIS, IS THE OVERVIEW_PATH. THE LAT/LNG FROM EACH IS STORED, AND WE CAN PULL WEATHER EVERY 30 POINTS (OR HOWEVER MANY)
+*
+*   THE FOLLOWING CODE RETRIEVES THE LAT & LNG DEPENDING ON THE INDEX OF THE OVERVIEW PATH.
+*   response['routes'][0]['overview_path']['INSERT INDEX HERE'].lat()
+*   response['routes'][0]['overview_path']['INSERT INDEX HERE'].lng()
+*
+*   TEXT HERE GIVES YOU '53.5mi', VALUE IS DISTANCE IN METERS e.g. '86050'
+*   response['routes'][0]['legs'][0]['distance']['text'] -OR- ['value']
+*
+*   SAME AS ABOVE, '57 mins' OR '3391' IN SECONDS
+*   response['routes'][0]['legs'][0]['duration']['text'] -OR- ['value']
+*
+*   THE FOLLOWING CODE TAKES THE OVERVIEW PATH AND STORES LAT & LNG POINTS EVERY 30 KEYS IN THE OVERVIEW PATH
+*   STORED IN THE weatherPoint OBJECT. CREATES NEW MARKERS ALONG ROUTE AND RENDERS MARKERS.
+*/
+
+function MapsService() {
+  this.directionParams = function(origin, destination, maps) {
+    return {
+      origin: origin,
+      destination: destination,
+      travelMode: maps.TravelMode.DRIVING,
+      optimizeWaypoints: true,
+      drivingOptions: {
+        //CREATE DATE INPUT FROM USER TO FILL THIS IN. MMDDYYYY NEEDED
+        departureTime: new Date('2016-07-29'),
+        trafficModel: maps.TrafficModel.PESSIMISTIC
+      }
+    }
+  }
+
+  this.createMarkers = function(response, maps, map) {
+    weatherPoint = {};
+    routePoints = response['routes'][0]['overview_path'];
+    // 'i' is the weatherPoint variable index, incremented by 1, 'j' is the index for each overview_path we're persisting.
+    i = 0;
+
+    for(j = 30; j < routePoints.length; j+=30) {
+      lat = routePoints[j].lat();
+      lng = routePoints[j].lng();
+      weatherPoint[i] = [routePoints[j].lat(), routePoints[j].lng()];
+      i++;
+
+      //creating new markers for each overview_path we're saving on the directionsDisplay variable.
+      new maps.Marker({
+        position: {lat, lng},
+        map: map
+      });
+    }
+  }
 }
 
 angular
