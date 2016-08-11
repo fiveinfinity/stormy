4653614f97c395392a005a757e3a4bfd5a15b449
fiveinfinity <jlachance1@gmail.com>
2016-08-10 21:39:22 -0500

    set cadence between markers

--- app/assets/javascripts/app/services/MapsService.js
+++ app/assets/javascripts/app/services/MapsService.js
@@ -33,20 +33,49 @@ function MapsService(TimeService, WeatherService) {
 
     //creates markers based on overview_path waypoints in the Google Maps Directions response.
     this.createMarkers = function(response, maps, map) {
-        markers = {};
-        routePoints = response['routes'][0]['overview_path'];
+        var markers = {};
+        //meters
+        var distanceInMeters = response['routes'][0]['legs'][0]['distance']['value'];
+        var distanceInMiles = toMiles(distanceInMeters);
+        var routePoints = response['routes'][0]['overview_path'];
+        //set routePoint cadence
+        var cadence = getCadence(distanceInMiles);
 
         var i = 0;
-        for (j = 30; j < routePoints.length; j += 30) {
+        for (j = cadence; j < routePoints.length; j += cadence) {
             var lat = routePoints[j].lat();
             var lng = routePoints[j].lng();
 
-            var geo = WeatherService.geoLookup(lat, lng, maps);
+            // var geo = WeatherService.geoLookup(lat, lng, maps);
             var marker = newMarker(lat, lng, map, maps);
-            markers[i] = [lat, lng, marker, geo];
+            //add 'geo' to the array below when app is ready.
+            markers[i] = [lat, lng, marker];
             i++;
         }
+        //the next lines to the return are for testing to keep API calls to a min, use the commented line for app.
+        var geo = WeatherService.geoLookup(markers[0][0], markers[0][1], maps);
+        geo.then(function(data) {
+            var city = data['data']['location']['city'];
+            var state = data['data']['location']['state'];
+            markers[0].push(city);
+            markers[0].push(state);
+        });
         return markers;
+    }
+
+    function toMiles(meters) {
+        return Math.floor(meters/1609);
+    }
+
+    function getCadence(miles) {
+        console.log(miles);
+        if(miles < 100) {
+            return 90;
+        } else if(miles > 100 && miles < 500) {
+            return 60;
+        } else {
+            return 30;
+        }
     }
 
 }
--- app/assets/javascripts/app/services/WeatherService.js
+++ app/assets/javascripts/app/services/WeatherService.js
@@ -1,14 +1,14 @@
 function WeatherService($http) {
     this.geoLookup = function(lat, lng, maps) {
-        var city, state
+        // var city, state
             // USING CORS.IO TO CIRCUMVENT CORS. NEED TO SET UP A PROXY. HEADERS NOT WORKING WITH WUNDERGROUND.
-            // return $http.get('http://cors.io/?u=http://api.wunderground.com/api/b924e5e377465063/geolookup/q/'+lat+','+lng+'.json').then(function(data) {
+            return $http.get('https://crossorigin.me/http://api.wunderground.com/api/b924e5e377465063/geolookup/q/'+lat+','+lng+'.json');
+            // .then(function(data) {
             //   city = data['data']['location']['city'];
             //   state = data['data']['location']['state'];
-            //   console.log(city+', '+state);
-            //   // return weatherPoint[i] = [city, state];
+            //   return data;
             // });
-            // return [city, state];
+
 
         // GOOGLE MAPS REVERSE GEO LOOKUP
         // var geocoder = new maps.Geocoder();
